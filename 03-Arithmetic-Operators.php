1930c0d7a13fdd281ba809d9af3cdea55a0ed259
Vitaly Lebedev <eslovo@gmail.com>
2019-06-04 17:01:08 +0300

    formating

---
+++ 03-Arithmetic-Operators.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by Vitaly Lebedev
+ * Email: eslovo@gmail.com
+ * Date: 04.06.2019
+ * Time: 16:13
+ */
+include_once dirname(__FILE__).'/layout/header.php';
+
+?>
+
+    <div class="w3-container w3-teal">
+        <h1>XXXXXXXX</h1>
+    </div>
+
+    <div class="w3-container">
+
+        <p>XXXXXXXX</p>
+        <ul class="w3-ul" style="width:100%">
+            <li>XXXXXXXX</li>
+            <li>XXXXXXXX</li>
+        </ul>
+
+        <p>XXXXXXXX</p>
+
+    </div>
+<?php
+
+include_once dirname(__FILE__).'/layout/footer.php';
